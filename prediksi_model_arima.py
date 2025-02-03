3dff73a8199ffe9a64476ec7ad4992b04ae781ba
mnaufalrifqi <mnaufalrifqi10@gmail.com>
2025-02-03 15:29:32 +0700

    Add files via upload

--- prediksi_model_arima.py
+++ prediksi_model_arima.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1ljZd1FdKAJu5LMrd16_a0IN5MHAoqOFH
 """
 
-
+import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
